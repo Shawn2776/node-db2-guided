660f393242e7a65e094fd5a6d586811381e45e5a
Dave Kidd <dave@davekidd.net>
2021-12-16 18:08:21 -0700

    wip

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -2,3 +2,5 @@ const knex = require("knex")
 const config = require("../knexfile.js")
 
 const db = knex(process.env.NODE_ENV === "production" ? config.production : config.development);
+
+module.exports = db
