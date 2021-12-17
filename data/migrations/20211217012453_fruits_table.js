2251851a4aa9876be718fa4e251c721bf8fbb15a
Dave Kidd <dave@davekidd.net>
2021-12-16 18:42:17 -0700

    wip

--- data/migrations/20211217012453_fruits_table.js
+++ data/migrations/20211217012453_fruits_table.js
@@ -3,6 +3,8 @@ exports.up = function(knex) {
     return knex.schema.createTable("fruits",tbl=>{
         tbl.increments() //primary key
         tbl.text("name",128).unique().notNullable()
+        tbl.decimal("avgWeightOz")
+        tbl.boolean("delicious")
     })
 };
 
