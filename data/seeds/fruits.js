bd8832038147d0a00102f30e833f424eac02da8c
Shawn <shawn.harrington2776@gmail.com>
2022-01-05 03:47:12 -0800

    wip

---
+++ data/seeds/fruits.js
@@ -0,0 +1,12 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('fruits').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('fruits').insert([
+        { fruit_name: 'apple', avg_weight: 10.5, is_delicious: true},
+        { fruit_name: 'orange', avg_weight: 4.5, is_delicious: false},
+        { fruit_name: 'strawberry', avg_weight: 1.5, is_delicious: true }
+      ]);
+    });
+};
