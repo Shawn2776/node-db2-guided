b0028cccfa1c56929d1b10def7c24d2a59eba1c8
Shawn <shawn.harrington2776@gmail.com>
2022-01-05 04:09:42 -0800

    successful migrate up

---
+++ data/migrations/20220105120124_add-color-to-fruits.js
@@ -0,0 +1,11 @@
+exports.up = function(knex) {
+  return knex.schema.table('fruits', tbl => {
+    tbl.string('fruit_color', 290);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.table('fruits', tbl => {
+    tbl.dropColumn('fruit_color');
+  });
+};
--- data/seeds/fruits.js
+++ data/seeds/fruits.js
@@ -6,7 +6,7 @@ exports.seed = function(knex) {
       return knex('fruits').insert([
         { fruit_name: 'apple', avg_weight: 10.5, is_delicious: true},
         { fruit_name: 'orange', avg_weight: 4.5, is_delicious: false},
-        { fruit_name: 'strawberry', avg_weight: 1.5, is_delicious: true }
+        { fruit_name: 'strawberry', avg_weight: 1.5, is_delicious: true, fruit_color: 'red' }
       ]);
     });
 };
